c900b78eeb6a26f74ce3d538f2f96508ebed44fe
kalou91 <plmlcu91@gmail.com>
2020-04-17 18:09:07 +0200

    log super

--- flask_app.py
+++ flask_app.py
@@ -113,10 +113,9 @@ def short_path():
         t0=time.perf_counter()
         start_coord, start_name, timing = civico2coord_first_result(G_list, da, civici_tpn, coords)
         stop_coord, stop_name, timing1 =  civico2coord_first_result(G_list, a, civici_tpn, coords)
-        logging.info('ci ho messo {t1} per trovare la stringa'.format(t1=timing[0]))
-        logging.info('ci ho messo {t2} per trovare il nodo'.format(t2=timing[1]))
-        logging.info('ci ho messo {t3} per trovare l\'indice'.format(t3=timing[2]))
-        logging.info('ci ho messo {tim} per trovare l\'indice'.format(tim=timing1))
+        logging.info('ci ho messo {t11} e {t12} per trovare la stringa'.format(t11=timing[0], t12=timing1[0]))
+        logging.info('ci ho messo {t21} e {t22} per trovare il nodo'.format(t21=timing[1], t22=timing1[1]))
+        logging.info('ci ho messo {t31} e {t32} per trovare l\'indice'.format(t31=timing[2], t32=timing1[2]))
         logging.info('ci ho messo {tot} a calcolare la posizione degli indirizzi'.format(tot=time.perf_counter() - t0))
         if request.form.get('meno_ponti'):
             f_ponti=True
