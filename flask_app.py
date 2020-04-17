e3204baafa2dc3a87af44c2ae0ec82c1c9a3f257
Alessandro Tonin <tonin.ale@gmail.com>
2020-04-17 14:06:12 +0200

    added webhook security

--- flask_app.py
+++ flask_app.py
@@ -3,12 +3,25 @@ import os
 import sys
 import logging
 import git
+import hmac
+import hashlib
+
 logging.basicConfig(filename='example.log',format='[%(asctime)s - %(filename)s:%(lineno)s - %(funcName)20s()] %(message)s',level=logging.DEBUG)
 sys.path.append('/home/rafiki/v4w/src')
 sys.path.append('/home/rafiki/v4w/libpy')
 import pyAny_lib
 from library_coords import civico2coord_first_result, civico2coord_find_address
 
+# check webhook github signature
+def is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=',1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin_1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+w_secret = os.getenv("WEBHOOK_SECRET_KEY")
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -27,13 +40,61 @@ logging.info("Carico i nodi")
 
 @app.route('/update_server',methods=['POST'])
 def webhook():
-    if request.method == 'POST':
+    if request.method != 'POST':
+        return 'OK'
+    else:
+        abort_code = 418
+        # Do initial validations on required headers
+        if 'X-Github-Event' not in request.headers:
+            abort(abort_code)
+        if 'X-Github-Delivery' not in request.headers:
+            abort(abort_code)
+        if 'X-Hub-Signature' not in request.headers:
+            abort(abort_code)
+        if not request.is_json:
+            abort(abort_code)
+        if 'User-Agent' not in request.headers:
+            abort(abort_code)
+        ua = request.headers.get('User-Agent')
+        if not ua.startswith('GitHub-Hookshot/'):
+            abort(abort_code)
+
+        event = request.headers.get('X-GitHub-Event')
+        if event == "ping":
+            return json.dumps({'msg': 'Hi!'})
+        if event != "push":
+            return json.dumps({'msg': "Wrong event type"})
+
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        # webhook content type should be application/json for request.data to have the payload
+        # request.data is empty in case of x-www-form-urlencoded
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(abort_code)
+
+        payload = request.get_json()
+        if payload is None:
+            print('Deploy payload is empty: {payload}'.format(
+                payload=payload))
+            abort(abort_code)
+
+        if payload['ref'] != 'refs/heads/master':
+            return json.dumps({'msg': 'Not master; ignoring'})
+
         repo = git.Repo('/home/rafiki')
         origin = repo.remotes.origin
-        origin.pull()
-        return 'Update PythonAnywhere succesfully', 200
-    else:
-        return 'Wrong event type', 400
+
+        pull_info = origin.pull()
+
+        if len(pull_info) == 0:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+        if pull_info[0].flags > 128:
+            return json.dumps({'msg': "Didn't pull any information from remote!"})
+
+        commit_hash = pull_info[0].commit.hexsha
+        build_commit = f'build_commit = "{commit_hash}"'
+        print(f'{build_commit}')
+        return 'Updated PythonAnywhere server to commit {commit}'.format(commit=commit_hash)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
