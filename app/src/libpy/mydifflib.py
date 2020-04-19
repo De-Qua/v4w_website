9f100b7bc3cd2ba9fe8816a0d213854c9ebecd52
lucatastrophe <luca.deltorre9@gmail.com>
2020-04-19 20:22:06 +0200

    changed ratio method

--- app/src/libpy/mydifflib.py
+++ app/src/libpy/mydifflib.py
@@ -1,6 +1,6 @@
 # mydifflib.py
 from difflib import SequenceMatcher
-from heapq import nlargest as _nlargest
+#from heapq import nlargest as _nlargest
 import numpy as np
 
 def get_close_matches_indexes(word, possibilities, n=3, cutoff=0.75):
@@ -21,19 +21,20 @@ def get_close_matches_indexes(word, possibilities, n=3, cutoff=0.75):
         raise ValueError("cutoff must be in [0.0, 1.0]: %r" % (cutoff,))
     result_ratio = []
     result_idx = []
+    # se vogliamo escludere le "junk sequence" (in questo caso lo spazio) SequenceMatcher(lambda x: x==" "),  
     s = SequenceMatcher()
     s.set_seq2(word)
     for idx, x in enumerate(possibilities):
         s.set_seq1(x)
-        #our_ratio = (s.find_longest_match(0, len(x), 0, len(word)).size) / len(word)
-        #if our_ratio >= cutoff:
-    #        result_ratio.append(our_ratio)
-    #        result_idx.append(idx)
-        if s.real_quick_ratio() >= cutoff and \
-           s.quick_ratio() >= cutoff and \
-           s.ratio() >= cutoff:
-            result_ratio.append(s.ratio())
+        our_ratio = (s.find_longest_match(0, len(x), 0, len(word)).size) / len(word)
+        if our_ratio >= cutoff:
+            result_ratio.append(our_ratio)
             result_idx.append(idx)
+    #    if s.real_quick_ratio() >= cutoff and \
+    #       s.quick_ratio() >= cutoff and \
+    #       s.ratio() >= cutoff:
+    #        result_ratio.append(s.ratio())
+    #        result_idx.append(idx)
 
     # Move the best scorers to head of list
     if not result_ratio:
