bfa0fde559c6c156a038f1b8f2f117222f5dae8b
lucatastrophe <luca.deltorre9@gmail.com>
2020-04-19 18:41:41 +0200

    boh

--- app/errors.py
+++ app/errors.py
@@ -5,6 +5,7 @@ from app import app
 def not_found_error(error):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
