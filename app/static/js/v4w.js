905c0865383ff52009e15991f640ea96ce1505bc
Pythonanywhere <tonin.al@gmail.com>
2020-05-05 10:27:52 +0000

    html changes

--- app/static/js/v4w.js
+++ app/static/js/v4w.js
@@ -4,102 +4,106 @@
   * Licensed under MIT
   */
 
-	/* Open a window visualizing a help message on how to make the correct search.
-		In the window a button to close it should be available, calling closeHelpWindow().
-		The actual happening is just making the element with id "helpwindow" visible.
-	*/
-	function showHelpWindow() {
-		document.getElementById("helpwindow").style.display = "block";
-		document.getElementById("searchbar").style.display = "none";
-		var thingstoBeHidden = document.getElementsByClassName("onlyMap");
-		for (i = 0; i < thingstoBeHidden.length; i++) {
-			thingstoBeHidden[i].style.display = "none";
-		}
+/* Open a window visualizing a help message on how to make the correct search.
+	In the window a button to close it should be available, calling closeHelpWindow().
+	The actual happening is just making the element with id "helpwindow" visible.
+*/
+function showHelpWindow() {
+	document.getElementById("helpwindow").style.display = "block";
+	document.getElementById("searchbar").style.display = "none";
+	var thingstoBeHidden = document.getElementsByClassName("onlyMap");
+	for (i = 0; i < thingstoBeHidden.length; i++) {
+		thingstoBeHidden[i].style.display = "none";
 	}
+}
 
-	/* Close the window visualizing a help message on how to make the correct search.
-		The actual happening is just making the element with id "helpwindow" invisible.
-	*/
-	function closeHelpWindow() {
-		document.getElementById("helpwindow").style.display = "none";
-		document.getElementById("searchbar").style.display = "block";
-		var thingstoBeShown = document.getElementsByClassName("onlyMap");
-		for (i = 0; i < thingstoBeShown.length; i++) {
-			thingstoBeShown[i].style.display = "inline";
-		}
+/* Close the window visualizing a help message on how to make the correct search.
+	The actual happening is just making the element with id "helpwindow" invisible.
+*/
+function closeHelpWindow() {
+	document.getElementById("helpwindow").style.display = "none";
+	document.getElementById("searchbar").style.display = "block";
+	var thingstoBeShown = document.getElementsByClassName("onlyMap");
+	for (i = 0; i < thingstoBeShown.length; i++) {
+		thingstoBeShown[i].style.display = "inline";
 	}
+}
 
-	/* Open a window visualizing a help message on how to make the correct search.
-		In the window a button to close it should be available, calling closeHelpWindow().
-		The actual happening is just making the element with id "helpwindow" visible.
-	*/
-	function showFeedbackWindow() {
-		document.getElementById("feedbackwindow").style.display = "block";
-		document.getElementById("searchbar").style.display = "none";
-		var thingstoBeHidden = document.getElementsByClassName("onlyMap");
-		for (i = 0; i < thingstoBeHidden.length; i++) {
-			thingstoBeHidden[i].style.display = "none";
-		}
+/* Open a window visualizing a help message on how to make the correct search.
+	In the window a button to close it should be available, calling closeHelpWindow().
+	The actual happening is just making the element with id "helpwindow" visible.
+*/
+function showFeedbackWindow() {
+	document.getElementById("feedbackwindow").style.display = "block";
+	document.getElementById("searchbar").style.display = "none";
+	var thingstoBeHidden = document.getElementsByClassName("onlyMap");
+	for (i = 0; i < thingstoBeHidden.length; i++) {
+		thingstoBeHidden[i].style.display = "none";
 	}
+}
 
-	/* Close the window visualizing a help message on how to make the correct search.
-		The actual happening is just making the element with id "helpwindow" invisible.
-	*/
-	function changeFeedbackandClose() {
-		console.log("Prima: ");
-		console.log(feedbackjs);
-		feedbackjs = 0;
-		console.log("Dopo: ")
-		console.log(feedbackjs);
-		toggleFeedbackWindowLayout();
-		closeFeedbackWindow();
+/* Close the window visualizing a help message on how to make the correct search.
+	The actual happening is just making the element with id "helpwindow" invisible.
+*/
+function changeFeedbackandClose() {
+	console.log("Prima: ");
+	console.log(feedbackjs);
+	feedbackjs = 0;
+	console.log("Dopo: ")
+	console.log(feedbackjs);
+	toggleFeedbackWindowLayout();
+	closeFeedbackWindow();
+}
+
+function toggleFeedbackWindowLayout() {
+	console.log(feedbackjs);
+	if (feedbackjs == 0) {
+		document.getElementById("formid").style.display = "block";
+		document.getElementById("grazieid").style.display = "none";
 	}
-
-	function toggleFeedbackWindowLayout() {
-		console.log(feedbackjs);
-		if (feedbackjs == 0) {
-			document.getElementById("formid").style.display = "block";
-			document.getElementById("grazieid").style.display = "none";
-		}
-		else if (feedbackjs == 1) {
-			showFeedbackWindow();
-			document.getElementById("formid").style.display = "none";
-			document.getElementById("grazieid").style.display = "block";
-		}
+	else if (feedbackjs == 1) {
+		showFeedbackWindow();
+		document.getElementById("formid").style.display = "none";
+		document.getElementById("grazieid").style.display = "block";
 	}
+}
 
-	function closeFeedbackWindow() {
-		document.getElementById("feedbackwindow").style.display = "none";
-		document.getElementById("searchbar").style.display = "block";
-		var thingstoBeShown = document.getElementsByClassName("onlyMap");
-		for (i = 0; i < thingstoBeShown.length; i++) {
-			thingstoBeShown[i].style.display = "inline";
-		}
+function closeFeedbackWindow() {
+	document.getElementById("feedbackwindow").style.display = "none";
+	document.getElementById("searchbar").style.display = "block";
+	var thingstoBeShown = document.getElementsByClassName("onlyMap");
+	for (i = 0; i < thingstoBeShown.length; i++) {
+		thingstoBeShown[i].style.display = "inline";
 	}
+}
 
+function searchAgain() {
+    document.getElementById("trovato").style.display = "none";
+	document.getElementById("searchbar").style.display = "block";
+}
 
-	function changeMap(currentMap) {
-		if (currentMap == "osm") {
-			mymap.attributionControl._attributions = {};
-			var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-				attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-				subdomains: 'abcd',
-				minZoom: 1,
-				maxZoom: 16,
-				ext: 'jpg'
-			}).addTo(mymap);
-			whichmap = "water";
-		}
-		else {
-			mymap.attributionControl._attributions = {};
-			var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-			maxZoom: 20,
-			zoomControl: false,
-			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-			}).addTo(mymap);
-			whichmap = "osm";
-		}
+function changeMap(currentMap) {
+	if (currentMap == "osm") {
+		mymap.attributionControl._attributions = {};
+		var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			subdomains: 'abcd',
+			minZoom: 1,
+			maxZoom: 16,
+			ext: 'jpg'
+		}).addTo(mymap);
+		whichmap = "water";
 	}
+	else {
+		mymap.attributionControl._attributions = {};
+		var OpenStreetMap_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+		maxZoom: 20,
+		zoomControl: false,
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(mymap);
+		whichmap = "osm";
+	}
+}
 
 function locateUser(map, marker, circle) {
 	map.locate({setView: false, watch: false}) /* This will return map so you can do chaining */
