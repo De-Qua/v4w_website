231e47a818e65f7ee46c0f4b5045e327ec9696b3
Alessandro Tonin <tonin.ale@gmail.com>
2020-07-19 21:10:01 +0200

    Riorganizzazione grafica dei bottoni della mappa

--- app/static/js/v4w.js
+++ app/static/js/v4w.js
@@ -94,7 +94,8 @@ function changeMap(currentMap) {
 		mymap.attributionControl._attributions = {};
 		var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 			attribution: 'DeQua | Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-			maxZoom: 13,
+			minZoom: 12,
+			maxZoom: 19,
 		}).addTo(mymap);
 		// var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 		// 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
