8b54853c50da94affe35ad7377ab53a519247ece
lucatastrophe <luca.deltorre9@gmail.com>
2020-09-16 13:54:58 +0200

    grafo acqueo aggiornato

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ import app.global_variables as global_variables
 folder = os.getcwd()
 folder_db = os.path.join(folder,"app","static","files")
 path_pickle_terra = os.path.join(folder_db,"dequa_ve_terra_v8_dequa_ve_terra_0509_pickle_4326VE")
-path_pickle_acqua = os.path.join(folder_db,"dequa_ve_acqua_v6_dequa_ve_acqua_0509_pickle_4326VE")
+path_pickle_acqua = os.path.join(folder_db,"dequa_ve_acqua_v7_1609_pickle_4326VE")
 
 # Logging
 app.logger.info("loading the graphs..")
