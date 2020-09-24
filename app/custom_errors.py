6d09fc01e55674be3ee6e63760f86a72d050e9d7
lucatastrophe <luca.deltorre9@gmail.com>
2020-09-25 01:25:41 +0200

    prova

---
+++ app/custom_errors.py
@@ -0,0 +1,7 @@
+# ci servono per cambiare i mesasggi di errore da Javascript
+# per ora non serve che facciano nulla
+class UserError(Exception):
+    pass
+
+class DeveloperError(Exception):
+    pass
