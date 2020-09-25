1b542c096b2ffb18778acc70eae1ad9974377516
Alessandro Tonin <tonin.ale@gmail.com>
2020-09-25 03:40:58 +0200

    Spostato bootstrap sotto a w3s, aggiornato colore bottone errore

--- app/static/js/v4w_js/init.js
+++ app/static/js/v4w_js/init.js
@@ -58,26 +58,33 @@ function initialize_html(){
     console.log("error: " + dict_in_JS.msg)
     document.getElementById("errorwindow-explanation").innerHTML = dict_in_JS.msg;
     var err_win = document.getElementById("errorwindow");
+    var err_btn = document.getElementById("errorFbButton");
     err_win.style.display = 'block';
     if (dict_in_JS.type == 'UserError') {
       if (err_win.classList.contains("alert-dq-red")){
         err_win.classList.remove("alert-dq-red");
+        err_btn.classList.remove("alert-dq-red");
       }
       err_win.classList.add("alert-dq-yellow");
+      err_btn.classList.add("alert-dq-yellow");
       document.getElementById("error-text").innerHTML = "Se pensi che non sia per questo che la ricerca non ha funzionato, lasciaci un feedback spiegandoci il problema!";
     }
     else if (dict_in_JS.type == 'DeveloperError') {
       if (err_win.classList.contains("alert-dq-yellow")) {
         err_win.classList.remove("alert-dq-yellow");
+        err_btn.classList.remove("alert-dq-yellow");
       }
       err_win.classList.add("alert-dq-red");
+      err_btn.classList.add("alert-dq-red");
       document.getElementById("error-text").innerHTML = "È un po' imbarazzante, ma questo è anche il motivo per cui la versione si chiama <strong>alpha</strong>!<br>Se vuoi lasciarci un feedback per darci qualche informazione in più, clicca qui:";
     }
     else {
       if (err_win.classList.contains("alert-dq-yellow")) {
         err_win.classList.remove("alert-dq-yellow");
+        err_btn.classList.remove("alert-dq-yellow");
       }
       err_win.classList.add("alert-dq-red");
+      err_btn.classList.add("alert-dq-red");
       document.getElementById("error-text").innerHTML = "È un po' imbarazzante, ma questo è anche il motivo per cui la versione si chiama <strong>alpha</strong>!<br>Se vuoi lasciarci un feedback per darci qualche informazione in più, clicca qui:";
     }
     //alert("Ahi ahi!!!\nOps... cossa xe nato :(\n"+dict_in_JS.msg)
