00681936b1e499f1d025139719836aa7778044dd
Alessandro Tonin <tonin.ale@gmail.com>
2020-10-28 01:57:26 +0100

    Aggiornati file copiando i file attualmente online per evitare conflitti

--- update_get_high_tide_level_time.sh
+++ update_get_high_tide_level_time.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pa_update_scheduled_task.py $1 -m $(date -d"$(date) + 5 minute" +"%M") -p
-echo "ora lo lanciamo manualmente noi - ciao pythonanywhere"
+echo "launching the python script for reading tide level.."
 python3 get_high_tide_level.py
-echo "è successo qualcosa?"
+echo "and updating time.."
+pa_update_scheduled_task.py $1 -m $(date -d"$(date) + 5 minute" +"%M") -p
+echo "updated scheduled task"
