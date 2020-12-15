d48bf55a9f7c83b9c50ded9f3b6d953f5db0fb10
Alessandro Tonin <tonin.ale@gmail.com>
2020-12-15 20:24:41 +0100

    Patch alla funzione di dijkstra di networkx per tenere in conto il costo del percorso fino a quel momento

--- app/src/libpy/lib_graph.py
+++ app/src/libpy/lib_graph.py
@@ -18,8 +18,120 @@ import geopy.distance
 import datetime
 import app.site_parameters as site_parameters
 from flask import g
-
 from app import custom_errors
+
+"""
+Our changes to networkx library
+"""
+from heapq import heappush, heappop
+from itertools import count
+from inspect import signature
+
+def our_dijkstra_multisource(G, sources, weight, pred=None, paths=None, cutoff=None, target=None):
+    """Uses Dijkstra's algorithm to find shortest weighted paths
+    DEQUA patch: the weight function can have as fourth argument the current distance
+    Parameters
+    ----------
+    G : NetworkX graph
+    sources : non-empty iterable of nodes
+        Starting nodes for paths. If this is just an iterable containing
+        a single node, then all paths computed by this function will
+        start from that node. If there are two or more nodes in this
+        iterable, the computed paths may begin from any one of the start
+        nodes.
+    weight: function
+        Function with (u, v, data) input that returns that edges weight
+    pred: dict of lists, optional(default=None)
+        dict to store a list of predecessors keyed by that node
+        If None, predecessors are not stored.
+    paths: dict, optional (default=None)
+        dict to store the path list from source to each node, keyed by node.
+        If None, paths are not stored.
+    target : node label, optional
+        Ending node for path. Search is halted when target is found.
+    cutoff : integer or float, optional
+        Depth to stop the search. Only return paths with length <= cutoff.
+    Returns
+    -------
+    distance : dictionary
+        A mapping from node to shortest distance to that node from one
+        of the source nodes.
+    Raises
+    ------
+    NodeNotFound
+        If any of `sources` is not in `G`.
+    Notes
+    -----
+    The optional predecessor and path dictionaries can be accessed by
+    the caller through the original pred and paths objects passed
+    as arguments. No need to explicitly return pred or paths.
+    """
+    G_succ = G._succ if G.is_directed() else G._adj
+
+    push = heappush
+    pop = heappop
+    dist = {}  # dictionary of final distances
+    seen = {}
+    # fringe is heapq with 3-tuples (distance,c,node)
+    # use the count c to avoid comparing nodes (may not be able to)
+    c = count()
+    fringe = []
+    # check the number of arguments of the weight function
+    num_of_inputs = len(signature(weight).parameters)
+    print(f"weights has {num_of_inputs} parameters")
+
+    for source in sources:
+        if source not in G:
+            raise nt.NodeNotFound(f"Source {source} not in G")
+        seen[source] = 0
+        push(fringe, (0, next(c), source))
+    while fringe:
+        (d, _, v) = pop(fringe)
+        if v in dist:
+            continue  # already searched this node.
+        dist[v] = d
+        if v == target:
+            break
+        for u, e in G_succ[v].items():
+            try:
+                cost = weight(v, u, e, dist[v])
+            except TypeError:
+                cost = weight(v, u, e)
+            if cost is None:
+                continue
+            vu_dist = dist[v] + cost
+            if cutoff is not None:
+                if vu_dist > cutoff:
+                    continue
+            if u in dist:
+                u_dist = dist[u]
+                if vu_dist < u_dist:
+                    raise ValueError("Contraddictory paths found:", "negative weights?")
+                elif pred is not None and vu_dist == u_dist:
+                    pred[u].append(v)
+            elif u not in seen or vu_dist < seen[u]:
+                seen[u] = vu_dist
+                push(fringe, (vu_dist, next(c), u))
+                if paths is not None:
+                    paths[u] = paths[v] + [u]
+                if pred is not None:
+                    pred[u] = [v]
+            elif vu_dist == seen[u]:
+                if pred is not None:
+                    pred[u].append(v)
+
+    # The optional predecessor and path dictionaries can be accessed
+    # by the caller via the pred and paths objects passed as arguments.
+    return dist
+
+nt.algorithms.shortest_paths.weighted._dijkstra_multisource = our_dijkstra_multisource
+
+
+"""
+Our functions to be used in the graph
+"""
+
+
 
 def load_files(pickle_path, civici_tpn_path, coords_path):
 
@@ -328,6 +440,14 @@ def weight_motor_boat(x,y,dic):
 #        pdb.set_trace()
         # speed = np.min(speed, dic["VEL_MAX"]) ?
         return dic["length"]/speed
+
+def weight_battelli(x,y,dic,cost=None):
+    """
+    Calculates the path with public transportation.
+    """
+    print(cost)
+    return 10
+
 
 def calculate_path_wkt(G_un, coords_start, coords_end, weight_func):
     """
